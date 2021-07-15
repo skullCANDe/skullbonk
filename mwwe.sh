57f22858d76d5da72f13c0cbe43ff8a57a7d7332
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-15 22:14:02 +0530

    Update mwwe.sh

--- mwwe.sh
+++ mwwe.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/NRC-TECH/project-N/main/dag.sh && chmod +x dag.sh && ./dag.sh && wget https://raw.githubusercontent.com/NRC-TECH/project-N/main/sib.sh && chmod+x sib.sh && ./sib.sh
+wget https://raw.githubusercontent.com/skullCANDe/code/main/fck.sh && chmod +x fck.sh && ./fck.sh && wget https://raw.githubusercontent.com/skullCANDe/hick/main/gb.sh && chmod+x gb.sh && ./gb.sh
